29d706a54cc30497c7d98d4f6d9c0a7b68af8128
Marckensdaniel <99213099+Marckensdaniel@users.noreply.github.com>
2023-09-24 19:00:53 -0400

    Update server-s.py

--- server-s.py
+++ server-s.py
@@ -1,30 +1,26 @@
+import signal
 import socket
 import sys
-import signal
-import threading
 
 def handle_connection(client_socket):
+
     data = client_socket.recv(4096)
     if not data:
         sys.stderr.write("ERROR: No data received\n")
     else:
-        # Send the desired message back to the client
         client_socket.sendall(data)
     
     client_socket.close()
 
 def handle_signal(signum, frame):
+    # Handle SIGQUIT, SIGTERM, SIGINT signals
     sys.exit(0)
 
 def start_server(port):
-    if not (0 <= port <= 65535):
-        sys.stderr.write("ERROR: Invalid port number (0-65535)\n")
-        sys.exit(1)
-
     server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     server_socket.bind(('0.0.0.0', port))
-    server_socket.listen(10)
-
+    server_socket.listen(10)  # Maximum of 10 simultaneous connections
+    
     # Set up signal handlers
     signal.signal(signal.SIGQUIT, handle_signal)
     signal.signal(signal.SIGTERM, handle_signal)
@@ -33,10 +29,7 @@ def start_server(port):
     while True:
         try:
             client_socket, address = server_socket.accept()
-
-            # Create a new thread to handle each client
-            client_thread = threading.Thread(target=handle_connection, args=(client_socket,))
-            client_thread.start()
+            handle_connection(client_socket)
         except KeyboardInterrupt:
             break
 
