a2e21bb8921999d4b556efb7f2a05c73a14921c6
Marckensdaniel <99213099+Marckensdaniel@users.noreply.github.com>
2023-09-24 18:20:30 -0400

    Update server-s.py

--- server-s.py
+++ server-s.py
@@ -18,7 +18,7 @@ def handle_signal(signum, frame):
 
 def start_server(port):
     server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    server_socket.bind(('131.94.128.43', 54634))
+    server_socket.bind(('0.0.0.0', port))
     server_socket.listen(10)
     
     # Set up signal handlers
